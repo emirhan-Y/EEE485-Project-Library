b00832b7650814ad9b7d7db8963c4d44da8a3a79
Emirhan Yagcioglu <emirhanyagcioglu20@gmail.com>
2024-03-15 15:43:10 +0300

    Test change
    -- test

--- Main.py
+++ Main.py
@@ -1,5 +1,47 @@
+import numpy as np
+from sklearn.linear_model import Ridge
+import matplotlib.pyplot as plt
+
 import util.LinR as LinR
 import util.LogR as LogR
 
 if __name__ == "__main__":
-    print(LinR.lin_r(), LogR.log_r())
+    test_X = np.array([[1, 1],
+                       [1, 2],
+                       [1, 3],
+                       [1, 4],
+                       [1, 5]])
+    test_y = np.array([0,
+                       2,
+                       4,
+                       6,
+                       8])
+    B_lin = LinR.lin_r(test_X, test_y)
+    B_ridge = LinR.ridge(test_X, test_y, 100)
+
+    y = np.array([0,
+                  2,
+                  4,
+                  6,
+                  8])
+    X = np.array([1,
+                  2,
+                  3,
+                  4,
+                  5]).reshape(-1, 1)
+    clf = Ridge(alpha=100.0)
+    clf.fit(X, y)
+
+    b_re = [clf.intercept_, clf.coef_]
+
+    print(B_lin, B_ridge, b_re, LogR.log_r())
+
+    # test plot
+    x = np.linspace(0, 10, 11)
+    y1 = clf.intercept_ + clf.coef_ * x
+    y2 = B_ridge[1] * x + B_ridge[0]
+
+    plt.scatter(X, y)
+    plt.plot(x, y1)
+    plt.plot(x, y2)
+    plt.show()
--- util/LinR.py
+++ util/LinR.py
@@ -1,2 +1,38 @@
-def lin_r():
-    return 0
+import numpy as np
+from numpy.linalg import inv
+
+
+def lin_r(X: np.array, y: np.array):
+    """
+    apply linear regression on a given set of data
+
+    :param X: is expected in the regular design matrix format X = (1_ x_1_ x_2_ ...) size = n x (p+1)
+    :param y: measured response vector (is a row vector by default by python, but in mathematical terms, it is a column vector)
+    :return: linear regression (the least squares) parameters
+    """
+    return np.matmul(np.matmul(inv(np.matmul(np.transpose(X), X)), np.transpose(X)), y)
+
+
+def ridge(X: np.array, y: np.array, lam: float):
+    """
+    apply ridge regression (L2 regularization) on a given set of data
+
+    :param X: is expected in the regular design matrix format X = (1_ x_1_ x_2_ ...) size = n x (p+1)
+    :param y: measured response vector (is a row vector by default by python, but in mathematical terms, it is a column vector)
+    :param lam: hyperparameter lambda of ridge regression
+    :return: the ridge regression parameters
+    """
+
+    return np.matmul(np.matmul(inv(np.matmul(np.transpose(X), X) + lam * np.identity(X.shape[1])), np.transpose(X)), y)
+
+
+def lasso(X: np.array, y: np.array, lam: float):
+    """
+    apply lasso regression (L1 regularization) on a given set of data
+
+    :param X: is expected in the regular design matrix format X = (1_ x_1_ x_2_ ...) size = n x (p+1)
+    :param y: measured response vector (is a row vector by default by python, but in mathematical terms, it is a column vector)
+    :param lam: hyperparameter lambda of lasso regression
+    :return: the lasso regression parameters
+    """
+    return np.matmul(np.matmul(inv(np.matmul(np.transpose(X), X)), np.transpose(X)), y)
