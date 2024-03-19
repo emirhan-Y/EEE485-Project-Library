e1fa46217d56654b3f5b7e0e0dc69144665d0d98
Emirhan Yagcioglu <emirhanyagcioglu20@gmail.com>
2024-03-20 02:16:10 +0300

    Docs update
    -- conf.py fix

--- docs/conf.py
+++ docs/conf.py
@@ -21,8 +21,13 @@ release = 'v0.1.'
 
 extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
 
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 
@@ -30,4 +35,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
